55b8fcd1cec6c4dea753808f38fe7bb77fb69038
Siddarth Krishna S <36948033+siddarthkrishna10@users.noreply.github.com>
2019-12-10 19:32:38 +0100

    Added code for pairplots

---
+++ Part I/Transfer_pairplots.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+#Reading the dataset into an object
+d = pd.read_csv('https://github.com/siddarthkrishna10/Football_Transfers/blob/master/Part%20I/Transfers.csv')
+
+#Cleaning the Dataset of NaN values
+d1 = d.dropna()
+
+#Plotting the data in pairplots using seaborn
+pp = sns.pairplot(d1, y_vars=['Age'], x_vars=['Market_value', 'Transfer_fee'],
+                  hue="Season", palette="Set2")
+plt.show()
